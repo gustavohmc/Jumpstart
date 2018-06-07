8c1b1aeaf8a00aa6cbe688fe7aa31f58c5288da4
GusCorrea <gus.vancouver@gmail.com>
2018-06-06 20:27:21 -0700

    final

--- resources/js/jump.js
+++ resources/js/jump.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
+
   $('.carousel').slick({
+    slidesToScroll: 1,
     accessibility: false,
     autoplay: true,
     autoplaySpeed: 4000,
@@ -8,4 +10,19 @@ $(document).ready(function(){
     pauseOnFocus: false,
     pauseOnHover: false
   });
+
+  var lastScrollTop = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - lastScrollTop > 50){
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      lastScrollTop = scrollTop;
+    } else if (lastScrollTop - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      lastScrollTop = scrollTop;
+    }
+  });
+
+
 });
